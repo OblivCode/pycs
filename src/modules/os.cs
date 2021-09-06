565932a9c55883dd8a1ab2ce836c89754daf521c
OblivCode <60195126+OblivCode@users.noreply.github.com>
2021-09-06 20:40:36 +0100

    Add files via upload

--- src/modules/os.cs
+++ src/modules/os.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static pycs.pycs;
 
 namespace pycs.modules
 {
@@ -13,32 +13,49 @@ namespace pycs.modules
         //enviroment variables
         public static void setenv(string key, string? value) => Environment.SetEnvironmentVariable(key, value);
         public static string? getenv(string key) => Environment.GetEnvironmentVariable(key);
-
-        public static string name()
+        public static Dictionary<string,string> environ
         {
-            return System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            get {
+                var envs = Environment.GetEnvironmentVariables();
+                return (Dictionary<string, string>)envs;
+            }
+        }
+
+        public static string name
+        {
+            get { return System.Runtime.InteropServices.RuntimeInformation.OSDescription; }
 
         }
-        public static void close(FileStream file) => file.Close();
         //dir
         public static string getcwd() => Directory.GetCurrentDirectory();
-        public static void chdir(string path) => Directory.SetCurrentDirectory(path);
+        public static void chdir(string path)
+        {
+            if (!Directory.Exists(path))
+                throw new FileExistsError("Directory does not exist.");
+
+            if (path == "..")
+                path = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            Directory.SetCurrentDirectory(path);
+        }
+        /// <exception cref="pycs.FileExistsError"></exception>
         public static void mkdir(string path)
         {
             if (Directory.Exists(path))
                 throw new FileExistsError("Directory already exists.");
             Directory.CreateDirectory(path);
         }
+        /// <exception cref="FileExistsError"></exception>
         public static void remove(string filename)
         {
             if (!File.Exists(filename))
-                throw new OSError("File does not exist.");
+                throw new FileExistsError("File does not exist.");
             File.Delete(filename);
         }
+        /// <exception cref="pycs.FileExistsError"></exception>
         public static void rmdir(string path)
         {
             if (!Directory.Exists(path))
-                throw new OSError("Directory does not exist.");
+                throw new FileExistsError("Directory does not exist.");
             Directory.Delete(path);
         }
         public static string[] listdir(string path)
@@ -57,63 +74,63 @@ namespace pycs.modules
             return files.ToArray();
         }
 
+       
+        public static string dirname(string path) => Path.GetDirectoryName(path);
+
+        //proc
+        public static int getpid() => Process.GetCurrentProcess().Id;
+        public static string system(string p, bool wait = false)
+        {
+            string args = "";
+            string[] split = p.Split(' ');
+            p = split[0];
+            if (split.Length > 1)
+                args = split.Where((s, i) => i != 0).ToString();
+            Process proc = Process.Start(p, args);
+
+            if (wait)
+            {
+                proc.WaitForExit();
+                return proc.StandardOutput.ReadToEnd();
+            }
+            else
+                return "";
+            
+        }
+
         public static class path
         { 
-            public static string join(string[] paths)
-            { 
-                string path = "/";
-                foreach (var p in paths)
-                {
-                    if (p[0] != '/' && path.Last() != '/')
-                        path += p;
-                    
-                }
-
-                return path;
+            public static string join(string a, string b)
+            {
+                if (a.LastOrDefault() != '/' && a.LastOrDefault() != '\\')
+                    a += '\\';
+                return Path.Combine(
+                    Path.GetDirectoryName(Path.GetFullPath(a)), b);
              
             }
 
             public static bool exists(string filename) => File.Exists(filename);
+
+            public static string basename(string path) => Path.GetFileName(path);
+                
+            /// <exception cref="pycs.OSError"></exception>
             public static long getsize(string filename)
             {
                 if (!File.Exists(filename))
                     throw new OSError("File does not exist.");
                 return new FileInfo(filename).Length;
             }
-        }
-
-        //exceptions
-        public class OSError : Exception
-        {
-            public OSError()
+            public static Tuple<string, string> split(string path)
             {
-            }
-
-            public OSError(string message)
-                : base(message)
-            {
-            }
-
-            public OSError(string message, Exception inner)
-                : base(message, inner)
-            {
+                int idx0 = path.LastIndexOf('/');
+                int idx1 = path.LastIndexOf('\\');
+                int idx = Math.Max(idx0, idx1);
+                string tail = path.Substring(idx);
+                string head = path.Substring(0, idx);
+                return Tuple.Create(head, tail);
             }
         }
-        public class FileExistsError : Exception
-        {
-            public FileExistsError()
-            {
-            }
 
-            public FileExistsError(string message)
-                : base(message)
-            {
-            }
-
-            public FileExistsError(string message, Exception inner)
-                : base(message, inner)
-            {
-            }
-        }
+       
     }
 }
