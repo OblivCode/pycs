52e4dcd6f9c2eba89ad30c57c381525775e6fe92
OblivCode <60195126+OblivCode@users.noreply.github.com>
2021-10-01 12:09:01 +0100

    Add files via upload

---
+++ Examples/str.cs
@@ -0,0 +1,22 @@
+using static pycs.pycs;
+using pycs.modules;
+using pycs.modules.zipfile;
+using pycs.modules.pathlib;
+using System.Threading.Tasks;
+
+namespace my_app
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int[] r = range(4, 10);
+
+            str print_array = "Array: {0}";
+            print_array.format(repr(r));
+            
+            print(print_array);
+        }
+    }
+}
